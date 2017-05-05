33e7b954bad46b81d2d2afb0bcd4631506e9e2cc
Carlos Vargas <carlosvargas@WorkMac.local>
2017-05-05 18:33:53 -0500

    merge

--- app/src/main/java/com/codemate/booklibrary/ui/activities/home/HomeActivity.java
+++ app/src/main/java/com/codemate/booklibrary/ui/activities/home/HomeActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 
 import com.codemate.booklibrary.R;
+import com.codemate.booklibrary.ui.activities.dialogmaterialsearchview.DialogMaterialSVActivity;
 import com.codemate.booklibrary.ui.activities.main.MainActivity;
 import com.codemate.booklibrary.ui.activities.materialsearchview.MaterialSVActivity;
 import com.codemate.booklibrary.ui.activities.toolbarsearchview.ToolbarSVActivity;
@@ -16,6 +17,7 @@ public class HomeActivity extends AppCompatActivity {
     private Button btnSearchView;
     private Button btnToolbarSearchView;
     private Button btnMaterialSearchView;
+    private Button btnDialogMaterialSearchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +27,7 @@ public class HomeActivity extends AppCompatActivity {
         btnSearchView = (Button) findViewById(R.id.btnSearchView);
         btnToolbarSearchView = (Button) findViewById(R.id.btnToolbarSearchView);
         btnMaterialSearchView = (Button) findViewById(R.id.btnMaterialSearchView);
+        btnDialogMaterialSearchView = (Button) findViewById(R.id.btnDialogMaterialSearchView);
 
         btnSearchView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,6 +47,13 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(HomeActivity.this, MaterialSVActivity.class));
+            }
+        });
+
+        btnDialogMaterialSearchView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this, DialogMaterialSVActivity.class));
             }
         });
     }
